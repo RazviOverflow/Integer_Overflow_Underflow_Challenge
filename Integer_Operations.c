e0782d22d4f1ab042d2bedf51cbdc698a333e497
RazviOverflow <razvioverflow@gmail.com>
2020-03-28 16:39:03 -0400

    Added fflushes after every print

--- Integer_Operations.c
+++ Integer_Operations.c
@@ -61,30 +61,38 @@ void level_1(){
 	int user_number;
 
 	printf("[+] Can you obtain a negative number adding a positive integer to %d?\n[+] Please insert a positive integer (decimal): ", number_1);
+	fflush(stdout);
 
 	// All numbers must be specified in decimal format
 	int scanf_ret_value = scanf("%d", &user_number);
 
 	if(scanf_ret_value <= 0){
 		printf("[!] You must insert a valid number!\n");
+		fflush(stdout);
 		exit(EXIT_FAILURE);
 	} else if(user_number < 0){
 		printf("[!] You must insert a positive number!\n");
+		fflush(stdout);
 		exit(EXIT_FAILURE);
 	} else if(errno == ERANGE) {
 		printf("[!] You inserted a huge number!\n     PLEASE THINK THE LOGIC BEHING THE OPERATIONS! :)\n");
+		fflush(stdout);
 		exit(EXIT_FAILURE);
 	} else {
 		printf("[✓] Number %d is positive.\n", user_number);
+		fflush(stdout);
 	}
 
 	int result = number_1 + user_number;
 	printf("[....] %d + %d = %d\n", number_1, user_number, result);
+	fflush(stdout);
 	if(result >= 0){
 		printf("[!] The result is not negative. Try again!\n");
+		fflush(stdout);
 		exit(EXIT_FAILURE);
 	} else {
 		printf("[✓] The result is %d. How's that even possible? Well... Let's move on.\n", result);
+		fflush(stdout);
 		print_separator();
 		sleep(1);
 	}
@@ -100,28 +108,36 @@ void level_2(){
 
 	printf("[+] Can you obtain a positive integer subtracting a negative one from %d\n", negative_number);
 	printf("[+] Please insert a negative integer (decimal): ");
+	fflush(stdout);
 	int scanf_ret_value = scanf("%d", &user_negative_number);
 
 	if(scanf_ret_value <= 0){
 		printf("[!] You must insert a valid number!\n");
+		fflush(stdout);
 		exit(EXIT_FAILURE);
 	} else if(user_negative_number >= 0){
 		printf("[!] You must insert a negative number!\n");
+		fflush(stdout);
 		exit(EXIT_FAILURE);
 	} else if(errno == ERANGE) {
 		printf("[!] You inserted a huge number!\n     PLEASE THINK THE LOGIC BEHING THE OPERATIONS! :)\n");
+		fflush(stdout);
 		exit(EXIT_FAILURE);
 	} else {
 		printf("[✓] Number %d is negative.\n", user_negative_number);
+		fflush(stdout);
 	}
 
 	int result = negative_number - user_negative_number;
 	printf("[....] %d + %d = %d\n", negative_number, user_negative_number, result);
+	fflush(stdout);
 	if(result <= 0){
 		printf("[!] The result is not positive. Try again!\n");
+		fflush(stdout);
 		exit(EXIT_FAILURE);
 	} else {
 		printf("[✓] The result is %d, and it's positive :)\n", result);
+		fflush(stdout);
 		print_separator();
 		sleep(1);
 	}
@@ -135,20 +151,25 @@ void level_3(){
 	long long int X, Y;
 
 	printf("[+] Insert X value (decimal):");
+	fflush(stdout);
 	int scanf_ret_value = scanf("%Ld", &X);
 
 	if(scanf_ret_value <= 0){
 		printf("[!] You must insert a valid number!\n");
+		fflush(stdout);
 		exit(EXIT_FAILURE);
 	} else if(X < 1337){
 		printf("[!] X must be bigger than 1337!\n");
+		fflush(stdout);
 		exit(EXIT_FAILURE);
 	} else {
 		printf("[✓] X is bigger than 1337!\n");
 		printf("[+] Insert Y value (decimal):");
+		fflush(stdout);
 		scanf("%Ld", &Y);
 		if(Y < 1337){
 			printf("[!] Y must be bigger than 1337!\n");
+			fflush(stdout);
 			exit(EXIT_FAILURE);
 		} else {
 			int truncated_X = X;
@@ -156,10 +177,12 @@ void level_3(){
 
 			if(truncated_X + truncated_Y != 1337){
 				printf("[!] X+Y doest not equal to 1337! Try again\n");
+				fflush(stdout);
 				exit(EXIT_FAILURE);
 			} else {
 				printf("[✓] X = %d, Y= %d; X+Y = 1337\n", truncated_X, truncated_Y);
 				printf("[✓] Great, you understand how truncation works! :)\n");
+				fflush(stdout);
 				print_separator();
 				sleep(1);
 			}
@@ -177,13 +200,16 @@ void level_4(){
 	int subtracted_number = 153153;
 
 	printf("[+] Insert X value (decimal): ");
+	fflush(stdout);
 	int scanf_ret_value = scanf("%Ld", &user_X);
 
 	if(scanf_ret_value <= 0){
 		printf("[!] You must insert a valid number!\n");
+		fflush(stdout);
 		exit(EXIT_FAILURE);
 	} else if(user_X <= 1337){
 		printf("[!] X must be bigger than %d!\n", 1337);
+		fflush(stdout);
 		exit(EXIT_FAILURE);
 	} else {
 
@@ -193,10 +219,12 @@ void level_4(){
 		if( user_result == expected_result){
 			printf("[✓] Good job! %Ld² is: %d (in 32 bits); then %Ld² - %d = %d\n", user_X, truncated_X, user_X, subtracted_number, expected_result);
 			printf("[✓] Great, you do have some knowledge about how integers and primitive data types behave at memory and cpu level! :)\n");
+			fflush(stdout);
 			print_separator();
 			sleep(1);
 		} else {
 			printf("[!] %Ld² - %d = %d which =/= %d! Try again\n", user_X, subtracted_number, user_result, expected_result);
+			fflush(stdout);
 			exit(EXIT_FAILURE);
 		}
 	}
@@ -272,6 +300,7 @@ printf("      \\___/|_|    \\__,_|\\__,_|\\__,_|_|\\__|_|\\___/|_| |_|        \n
 	printf("[+] Turns out you're back in school, learning what sums are.\n");
 	printf("[+] There is a rumor going around about two positive integers resulting in a negative one after adding them up.\n");
 	printf("[+] Could it be true?\n");
+	fflush(stdout);
 }
 
 void print_level_2(){
@@ -291,7 +320,9 @@ void print_level_2(){
 	printf("\n");                                                                                                                                     
 	printf("[+] Allright! Good job :)\n");
 	printf("[+] Now, is it possible to obtain a positive integer from subtracting two negative ones?\n");
+	fflush(stdout);
 }
+
 
 void print_level_3(){
 
@@ -315,6 +346,7 @@ void print_level_3(){
 	printf("[+] Now let us learn about some simple equations.\n");
 	printf("[+] Solve the following equation:\n");
 	printf("[+] X + Y = 1337 where X > 1337 and Y > 1337\n");
+	fflush(stdout);
 }
 
 void print_level_4(){
@@ -342,6 +374,7 @@ void print_level_4(){
 	// x valid solution = 132654
 	//132654^2 = 17597083716
 	//17597083716 in 32 bits is 417214532; 417214532 - 153153 = 417061379
+	fflush(stdout);
 
 }
 
